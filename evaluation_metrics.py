dd91f9112477485ccc907797e71e7f91a0114345
Nikhil Reddy <nikhilreddytn@gmail.com>
2025-03-15 22:41:38 -0400

    updated metrics file

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -3,6 +3,17 @@ import json
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 def standardize(str1, str2, stops):
     str1 = str1.lower()
@@ -26,22 +37,28 @@ def standardize(str1, str2, stops):
     return word1, word2
 
 
-def metrics(json1, json2):
+def metrics(answers, expected):
     nltk.download('stopwords')
     nltk.download('punkt_tab')
     stops = set(stopwords.words('english'))
 
-    with open(json1, "r", encoding="utf-8") as file1:
+    with open(answers, "r", encoding="utf-8") as file1:
         answers = json.load(file1)
-    with open(json2, "r", encoding="utf-8") as file2:
+    with open(expected, "r", encoding="utf-8") as file2:
         expected = json.load(file2)
     
     exact_matches = 0
     f1_scores = []
     ans_recall = []
     total_checks = len(expected)
+
+    ans_embeddings = []
+
     for num in answers:
-        # print(expected[num], answers[num])
+        ans_embed = model.encode(answers[num], convert_to_tensor=True)
+        ref_embed = model.encode(expected[num][0], convert_to_tensor=True)
+        ans_embeddings.append(util.pytorch_cos_sim(ref_embed, ans_embed).item())
+
         word1, word2 = standardize(answers[num], expected[num][0], stops)
         
         overlapping = 0
@@ -59,8 +76,9 @@ def metrics(json1, json2):
         f1 = (2 * precision * recall) / (precision + recall) if precision + recall != 0 else 0
         f1_scores.append(f1)
     
-    em = exact_matches / total_checks
     avg_f1_scores = sum(f1_scores) / len(f1_scores) if f1_scores else 0
     avg_recall = sum(ans_recall) / len(ans_recall) if ans_recall else 0
 
-    return avg_recall, avg_f1_scores, em
+    return avg_recall, avg_f1_scores, sum(ans_embeddings)/len(ans_embeddings) 
+
+print(metrics("metric_answers.json","selected_answers.txt"))
